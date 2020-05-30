4d8a7892ae6b1be721b193fa30b1697b4b01b713
Neuber Oliveira <neuber.oliveira05@gmail.com>
2020-05-30 19:31:51 -0300

    :art: update to full project using vue-cli

--- README.md
+++ README.md
@@ -1,14 +1,39 @@
-Part of main project for [micro-frameworks](https://github.com/multisolution/ops/issues/29)
+# micro-vue
 
-To test events on console 
+## Project setup
 
-***test send***
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+
+```
+yarn build
+```
+
+### Lints and fixes files
+
+```
+yarn lint
+```
+
+### Customize configuration
+
+See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### Test events
+
+**test send**
 
 `window.addEventListener('increment', (ev)=>console.log(ev.detail) )`
 
-
-
-***test receive***
+**test receive**
 
 `var event = new CustomEvent('increment_evt', {detail: {target: 'vue'}}); window.dispatchEvent(event)`
-
---
+++ src/App.vue
@@ -0,0 +1,83 @@
+<template>
+  <div id="app">
+    <div id="counter-container">
+      <div v-on:click="decrement" class="button counter-control">-</div>
+      <div>{{counter}}</div>
+      <div v-on:click="increment" class="button counter-control">+</div>
+      <div id="dispatch" class="button" v-on:click="dispatch">Dispatch</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data: function() {
+    return {
+      counter: 0
+    };
+  },
+  created: function() {
+    window.addEventListener("increment_evt", evt => {
+      this.counter++;
+      console.log("event received", evt.detail, this);
+    });
+  },
+  methods: {
+    increment: function() {
+      this.counter++;
+    },
+    decrement: function() {
+      this.counter--;
+    },
+    dispatch: function() {
+      var payload = {
+        target: "vue"
+      };
+      var event = new CustomEvent("increment", {
+        detail: payload
+      });
+      window.dispatchEvent(event);
+      console.log("event sent", payload);
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+#counter-container {
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  width: 150px;
+}
+
+.button {
+  font-size: 24px;
+  font-weight: bold;
+  color: #fff;
+  background-color: cornflowerblue;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 36px;
+  height: 36px;
+  cursor: pointer;
+}
+
+#dispatch {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 150px;
+  margin-top: 10px;
+}
+</style>
